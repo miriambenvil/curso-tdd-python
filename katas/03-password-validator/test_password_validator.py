e9ed44c1b6b185893b3e0d537457bb91c4e30b9b
Antonio de la Torre <antonio@codesai.com>
2021-01-10 23:29:19 +0100

    Remove unused method.

--- katas/03-password-validator/test_password_validator.py
+++ katas/03-password-validator/test_password_validator.py
@@ -4,7 +4,3 @@ import unittest
 class PasswordValidatorTest(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
