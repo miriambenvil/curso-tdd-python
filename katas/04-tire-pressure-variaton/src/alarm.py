b588e05203c08b3f323c7215b3bb4cf72770d5e5
Antonio de la Torre <antonio@codesai.com>
2021-01-10 22:54:09 +0100

    Refactor. Rename method.

--- katas/04-tire-pressure-variaton/src/alarm.py
+++ katas/04-tire-pressure-variaton/src/alarm.py
@@ -16,16 +16,16 @@ class Alarm:
         if (psi_pressure_value < self._LowPressureThreshold) or (self._HighPressureThreshold < psi_pressure_value):
             if not self._is_alarm_on():
                 self._alarm_on = True
-                self._display.print_message("Alarm activated!")
+                self._display.print("Alarm activated!")
 
         else:
             if self._is_alarm_on():
-                self._display.print_message("Alarm deactivated!")
+                self._display.print("Alarm deactivated!")
 
     def _is_alarm_on(self):
         return self._alarm_on
 
 
 class Display:
-    def print_message(self, msg):
+    def print(self, msg):
         print(msg)
