62e7e34952f6315d166a5e65bc8366c1dff7758a
Antonio de la Torre <antonio@codesai.com>
2021-01-10 22:31:37 +0100

    Remove unused function.

--- katas/04-tire-pressure-variaton/src/test_alarm.py
+++ katas/04-tire-pressure-variaton/src/test_alarm.py
@@ -10,7 +10,3 @@ class AlarmTest(unittest.TestCase):
     def test_run_alarm(self):
         self._alarm.check()
         self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
